172d7fb24c1d29a2856e9967e584de447a3d96ff
Kasey <kasey.murdoch@yahoo.com>
2022-02-01 09:04:59 -0800

    Button now resets game with player chosen grid size

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 const container = document.querySelector(`.container`)
 const button = document.querySelector(`button`)
 
+// Function creates the grid of divs and assigns them to classes
 function createGrid (columnAmount, rowAmount) {
     for (i = 0; i < columnAmount; i++) {
         const column = document.createElement(`div`)
@@ -13,16 +14,34 @@ function createGrid (columnAmount, rowAmount) {
 }
 createGrid(16, 16)
 
-const boxNode = document.querySelectorAll(`.boxClass`)
+// const columnNode = document.querySelectorAll(`.columnClass`)
+let boxNode = document.querySelectorAll(`.boxClass`)
 
+// Iterates through box node list and for every box allow mouseover to
+// change the color of said box
 boxNode.forEach((e) => {
     e.addEventListener('mouseover', () => {
         e.style.backgroundColor = `purple`
     })
 })
 
+
+// Button resets game. Changing the grid size is WIP
 button.addEventListener('click', () => {
     boxNode.forEach((e) => {
         e.style.backgroundColor = ``
+    })
+    let reset = prompt(`Enter new grid size`, `16`)
+    if (reset > 100) {
+        alert(`100 maximum grid size! Choose again`)
+        reset = prompt(`Enter new grid size`, `16`)
+    }
+    container.innerHTML = '';
+    createGrid(reset, reset)
+    boxNode = document.querySelectorAll(`.boxClass`)
+    boxNode.forEach((e) => {
+        e.addEventListener('mouseover', () => {
+            e.style.backgroundColor = `purple`
+        })
     })
 })
